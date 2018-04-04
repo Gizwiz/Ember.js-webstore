7e4daead8c42cc20e47d43139b812788ba088609
Gizwiz <markus.sahrakorpi@gmail.com>
2018-04-04 20:38:09 +0300

    index.hbs improvements

--- app/components/bike-categories.js
+++ app/components/bike-categories.js
@@ -13,12 +13,21 @@ export default Component.extend({
             this.get('router').transitionTo('/');
         },
         populateDropdown(cats) {
+            document.getElementById('ddc').style.display = 'block';
             document.getElementById('link-list').innerHTML = "<h3>" + cats.name + "</h3>";
 
             for (var i = 0; i < cats.subcategories.length; i++) {
                 document.getElementById('link-list').innerHTML += "<a>" + cats.subcategories[i] + "</a>";
             }
             document.getElementById('link-list-image-container').innerHTML = "<img class='link-list-image' src='"+cats.image+"' alt=''>";
+        },
+        closeDropdown(category){
+            console.log("close dropdown");
+            document.getElementById('ddc').style.display = 'none';
+        },
+        openDropdown(){
+            console.log("open dropdown");
+            document.getElementById('ddc').style.display = 'block';
         }
     }
 });
--- app/components/item-showcase.js
+++ app/components/item-showcase.js
@@ -1,4 +1,19 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
 
 export default Component.extend({
+    router: service(),
+    queryParams: {
+        category: {
+            refreshModel: true
+        }
+    },
+    actions: {
+        display(item) {
+            console.log(item._id);
+            var item_id = item._id;
+            // this.get('router').transitionTo('/item', { queryParams: { id: item_id } });
+            this.get('router').transitionTo('/item?id=' + item_id);
+        }
+    }
 });
